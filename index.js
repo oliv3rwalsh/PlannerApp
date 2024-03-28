41cedda3e0c3cc320355810ff22d2030092777a1
Oliver Walsh <oliverwalsh45@gmail.com>
2024-03-27 22:47:30 -0500

    added buttons

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// <!-- <div class="add-new-btn">
+// <i class="fa-regular fa-calendar-plus"></i>
+// <p>Add New</p>
+// </div> -->
+
 function buildbody(){
     var extcontentbox = document.querySelector("#content-box");
     // clear
@@ -8,16 +13,38 @@ function buildbody(){
     i = 0;
     while(i < 7){
         // create elements
+
+        // column
         var col = document.createElement("div");
         col.className = "content";
         col.id = "generated-col-" + days[i].toLowerCase();
+
+        // header
         var head = document.createElement("div");
         head.className = "content-header";
         var type = document.createElement("p");
         type.innerHTML = days[i];
-        // build column
         head.appendChild(type);
+
+        var content = document.createElement("div");
+        content.id = "content-" + days[i].toLowerCase();
+        content.className = "content-container";
+
+        // button
+        var btn = document.createElement("div");
+        btn.className = "add-new-btn"
+        btn.id = "add-" + days[i].toLowerCase();
+        var cal = document.createElement("i");
+        cal.className = "fa-regular fa-calendar-plus";
+        var p = document.createElement("p");
+        p.innerHTML = "Add New"
+        btn.appendChild(cal)
+        btn.appendChild(p)
+
+        // build column
         col.appendChild(head);
+        col.appendChild(content);
+        col.appendChild(btn);
         // send to container
         extcontentbox.appendChild(col);
         // iterate
@@ -26,11 +53,12 @@ function buildbody(){
     readin("http://localhost:8000/OneDrive/Documents/PlannerApp/assignments.csv");
 }
 
-function addassignment(c, d, dest){
+function addassignment(c, d, dest, index=0){
     var assn = document.createElement("div");
     assn.className = "assignment";
     var bdy = document.createElement("p");
     bdy.innerHTML = "<span class =\"class-name\">" + c + "</span> " + d;
+    bdy.id = "generated-assignment-" + index;
     assn.appendChild(bdy);
     document.querySelector(dest).appendChild(assn);
 
@@ -41,8 +69,8 @@ function parse(t){
     i = 0
     while(i < assignments.length-1){
         const comps = assignments[i].split(",")
-        dest = "#generated-col-"+comps[2].toLowerCase();
-        addassignment(comps[0].toUpperCase(), comps[1], dest);
+        dest = "#content-"+comps[2].toLowerCase();
+        addassignment(comps[0].toUpperCase(), comps[1], dest, i);
         i++
     }
 }
@@ -52,6 +80,7 @@ function readin(path){
   .then((res) => res.text())
   .then((text) => {
     parse(text);
+    console.log("read")
    })
   .catch((e) => console.error(e));
 }
